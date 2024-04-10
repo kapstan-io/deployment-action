ca2d28a60986b7b3cb7df17e64cc8a5da067fea1
harshit-kapstan <harshit@kapstan.io>
2024-04-10 17:52:28 +0530

    KAP-2247: add github action for CD

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 set -e
 
-api_url="https://pokeapi.co/api/v2/pokemon/${INPUT_POKEMON_ID}"
-echo $api_url
+kapstan_deployment_trigger_url="https://api.kapstan.io/v1/applications/${INPUT_APPLICATION_ID}/deploy"
+echo "prepared-url: $kapstan_deployment_trigger_url"
 
-pokemon_name=$(curl "${api_url}" | jq ".name")
-echo $pokemon_name
+request_body=$(cat <<EOF
+{
+  "imageTag": "$INPUT_IMAGE_TAG",
+  "imageRepositoryName": "$INPUT_IMAGE_REPOSITORY_NAME",
+  "comment": "Reason: Trigger by action ${GITHUB_EVENT_NAME} on ${GITHUB_REF_NAME} in ${GITHUB_REPOSITORY}",
+}
+EOF
+)
 
-echo "::set-output name=pokemon_name::$pokemon_name"
+echo "request_body: $request_body"
+
+echo "making API call to Kapstan"
+status_code=$(curl -k -o /dev/null -w "%{http_code}" -X POST "$kapstan_deployment_trigger_url" \
+  -H "Content-Type: application/json" \
+  -H "x-api-key: $INPUT_KAPSTAN_API_KEY" \
+  -d "$request_body")
+
+echo "got response from Kapstan: $status_code"
