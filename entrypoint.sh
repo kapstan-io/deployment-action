cf98614405a9e649dc43b63f844f6c0c11aeb475
Aashutosh Khandelwal <167970586+aashutosh-kapstan@users.noreply.github.com>
2024-09-10 09:51:42 +0530

    Fix predeploy tag translation

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ deployment_application() {
 { 
   "imageTag": "$INPUT_IMAGE_TAG",
   "comment": "Deployment triggered by action ${GITHUB_EVENT_NAME} on ${GITHUB_REF_NAME} in ${GITHUB_REPOSITORY}",
-  "preDeployImageTag": "$PRE_DEPLOY_IMAGE_TAG"
+  "preDeployImageTag": "$INPUT_PRE_DEPLOY_IMAGE_TAG"
 }
 EOF
 )
